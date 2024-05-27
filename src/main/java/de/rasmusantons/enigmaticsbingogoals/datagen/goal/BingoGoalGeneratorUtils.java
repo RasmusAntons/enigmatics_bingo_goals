50b46201f97d383b6138f815326319b554e6a4c3
Catz-uwu <65136376+Catz-uwu@users.noreply.github.com>
2024-05-28 01:00:02 +0300

    added all get single advancement goals

--- src/main/java/de/rasmusantons/enigmaticsbingogoals/datagen/goal/BingoGoalGeneratorUtils.java
+++ src/main/java/de/rasmusantons/enigmaticsbingogoals/datagen/goal/BingoGoalGeneratorUtils.java
@@ -2,10 +2,25 @@ package de.rasmusantons.enigmaticsbingogoals.datagen.goal;
 
 import com.mojang.authlib.properties.Property;
 import com.mojang.authlib.properties.PropertyMap;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.component.DataComponents;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.game.ClientboundLevelChunkPacketData.BlockEntityTagOutput;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.util.Unit;
+import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.component.ResolvableProfile;
+import net.minecraft.world.level.block.entity.BannerPatternLayers;
+import net.minecraft.world.level.block.entity.BannerPatterns;
+import net.minecraft.world.level.block.entity.BlockEntity;
 
 import java.util.Optional;
 
@@ -38,5 +53,14 @@ public class BingoGoalGeneratorUtils {
         return stack;
     }
 
-    ;
+    public static ItemStack getOminousBanner(HolderLookup.Provider registries) {
+        var patternRegistry = registries.lookupOrThrow(Registries.BANNER_PATTERN);
+        
+        ItemStack itemStack = new ItemStack(Items.WHITE_BANNER);
+        BannerPatternLayers bannerPatternLayers = new BannerPatternLayers.Builder().addIfRegistered(patternRegistry, BannerPatterns.RHOMBUS_MIDDLE, DyeColor.CYAN).addIfRegistered(patternRegistry, BannerPatterns.STRIPE_BOTTOM, DyeColor.LIGHT_GRAY).addIfRegistered(patternRegistry, BannerPatterns.STRIPE_CENTER, DyeColor.GRAY).addIfRegistered(patternRegistry, BannerPatterns.BORDER, DyeColor.LIGHT_GRAY).addIfRegistered(patternRegistry, BannerPatterns.STRIPE_MIDDLE, DyeColor.BLACK).addIfRegistered(patternRegistry, BannerPatterns.HALF_HORIZONTAL, DyeColor.LIGHT_GRAY).addIfRegistered(patternRegistry, BannerPatterns.CIRCLE_MIDDLE, DyeColor.LIGHT_GRAY).addIfRegistered(patternRegistry, BannerPatterns.BORDER, DyeColor.BLACK).build();
+        itemStack.set(DataComponents.BANNER_PATTERNS, bannerPatternLayers);
+        itemStack.set(DataComponents.HIDE_ADDITIONAL_TOOLTIP, Unit.INSTANCE);
+        itemStack.set(DataComponents.ITEM_NAME, Component.translatable("block.minecraft.ominous_banner").withStyle(ChatFormatting.GOLD));
+        return itemStack;
+    }
 }
