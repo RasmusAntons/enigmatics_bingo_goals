3e042685eb3ddb4fad338e50f3bcdc6365826f25
owl <mail@rasmusantons.de>
2024-06-15 14:33:51 +0200

    throw exception if our datagen runs before bingo platform initialized

--- src/main/java/de/rasmusantons/enigmaticsbingogoals/EnigmaticsBingoGoals.java
+++ src/main/java/de/rasmusantons/enigmaticsbingogoals/EnigmaticsBingoGoals.java
@@ -2,6 +2,7 @@ package de.rasmusantons.enigmaticsbingogoals;
 
 import de.rasmusantons.enigmaticsbingogoals.conditions.EnigmaticsBingoGoalsConditions;
 import de.rasmusantons.enigmaticsbingogoals.triggers.EnigmaticsBingoGoalsTriggers;
+import io.github.gaming32.bingo.platform.BingoPlatform;
 import net.fabricmc.api.ModInitializer;
 
 public class EnigmaticsBingoGoals implements ModInitializer {
@@ -9,6 +10,8 @@ public class EnigmaticsBingoGoals implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        if (BingoPlatform.platform == null)
+            throw new RuntimeException("Bingo mod isn't initialized???");
         EnigmaticsBingoGoalsConditions.load();
         EnigmaticsBingoGoalsTriggers.load();
     }
