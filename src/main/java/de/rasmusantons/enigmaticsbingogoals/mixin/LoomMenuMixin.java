151e4a8d77be3634ea69dece26bfeab319716ed1
ty7e <61668943+ty7e@users.noreply.github.com>
2024-08-10 16:48:34 +0200

    Use less expensive and more OO way of accessing Outer scope.

--- src/main/java/de/rasmusantons/enigmaticsbingogoals/mixin/LoomMenuMixin.java
+++ src/main/java/de/rasmusantons/enigmaticsbingogoals/mixin/LoomMenuMixin.java
@@ -2,31 +2,31 @@ package de.rasmusantons.enigmaticsbingogoals.mixin;
 
 import de.rasmusantons.enigmaticsbingogoals.triggers.EnigmaticsBingoGoalsTriggers;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.Container;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.ContainerLevelAccess;
 import net.minecraft.world.inventory.LoomMenu;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.lang.reflect.Field;
 
 
 @Mixin(targets = "net.minecraft.world.inventory.LoomMenu$6")
 public abstract class LoomMenuMixin  {
 
+    @Unique
+    private LoomMenu loomMenu;
+
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void init(LoomMenu loomMenu, Container container, int i, int j, int k, ContainerLevelAccess containerLevelAccess, CallbackInfo ci) {
+        this.loomMenu = loomMenu;
+    }
+
     @Inject(method = "onTake", at = @At("HEAD"))
-    private void onTake(Player player, ItemStack stack, CallbackInfo ci) throws NoSuchFieldException, IllegalAccessException {
-
-        LoomMenu loomMenu = null;
-
-        for (Field field : this.getClass().getDeclaredFields()) {
-            if (field.getType() == LoomMenu.class) {
-                field.setAccessible(true);
-                loomMenu = (LoomMenu) field.get(this);
-            }
-        }
+    private void onTake(Player player, ItemStack stack, CallbackInfo ci) {
 
         if (loomMenu != null) {
             EnigmaticsBingoGoalsTriggers.USE_LOOM.get().trigger(
